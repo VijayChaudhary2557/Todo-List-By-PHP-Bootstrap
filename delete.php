342df9fc290141590a1b03d3e2ceed90ebeb31a3
VijayChaudhary2557 <155866301+VijayChaudhary2557@users.noreply.github.com>
2025-05-17 22:02:46 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect('localhost','root','','php');
+    $conn = mysqli_connect("sql12.freesqldatabase.com","sql12779399","kA4qNbKN3B","sql12779399");
 
     $id = $_GET['id'];
 
